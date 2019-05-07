bec8a391da01691f65c1b8ce5efda55efbe6302d
Mahaveer Agarwal <mahaveer.agarwal@nagarro.com>
2019-05-07 17:44:53 +0530

    Completed the module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -7,11 +7,15 @@ class QuizQuestion extends Component {
     constructor (props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
+        this.state = {'incorrectAnswer': false};
     }
 
     handleClick (buttonText) {
         if(this.props.quiz_question.answer === buttonText) {
             this.props.showNextQuestionHandler(buttonText);
+            this.setState({'incorrectAnswer': false});
+        } else {
+            this.setState({'incorrectAnswer': true});
         }
     }
 
@@ -25,6 +29,7 @@ class QuizQuestion extends Component {
             {this.props.quiz_question.answer_options.map( (answer_option, index) => <QuizQuestionButton  clickHandler = {this.handleClick} key={index} button_text = {answer_option} /> )}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <div className="error">Sorry, That is not the right answer</div> : null }
       </main>
     }
 }

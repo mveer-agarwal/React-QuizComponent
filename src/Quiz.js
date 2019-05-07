aa82cd7f3c95c9a067fedda3f0764f1756d3184f
Mahaveer Agarwal <mahaveer.agarwal@nagarro.com>
2019-05-07 16:01:11 +0530

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
 
@@ -11,8 +12,10 @@ class Quiz extends Component {
     }
 
     render () {
+        const isQuizEnd = quizData.quiz_questions.length -1  === this.state.quiz_position - 1;
+
         return <div>
-            <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position -1] }/>
+            { isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position -1] }/> }
         </div>
     };
 }

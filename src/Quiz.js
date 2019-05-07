e2b388a17d3430bfb02baee52fad553c68cd1c03
Mahaveer Agarwal <mahaveer.agarwal@nagarro.com>
2019-05-07 14:54:51 +0530

    Adding QuizQuestion class

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,10 @@ class Quiz extends Component {
     }
 
     render () {
-        return <div className="quiz_question">{quizData.quiz_questions[0].instruction_text}</div>
+        return <div>
+            <div>Quiz</div>
+            <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+        </div>
     };
 }
 
